53a14e4dd3d72594646dd4ccc9261318608f8d08
Matthew Riley <matthewriley@interapts-macbook-pro-10.local>
2018-08-18 17:24:11 -0500

    Initial Commit

---
+++ GuessingGame1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GuessingGame1
+//
+//  Created by Matthew Riley on 8/18/18.
+//  Copyright © 2018 Matthew Riley. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
